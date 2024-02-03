a8633a571f00dfb1c3dbfa9986e93da6cad2bd33
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2024-02-03 03:29:06 +0300

    add skills page

--- src/layout/About/About.tsx
+++ src/layout/About/About.tsx
@@ -1,6 +1,45 @@
 function About(){
     return(
-        <div>About</div>
+        <div>
+            <div>
+                <h2>About Me</h2>
+                <p>The Generator App is an online tool that helps you to export ready-made templates ready to work as your future website. It helps you to combine slides, panels and other components and export it as a set of static files: HTML/CSS/JS.</p>
+            </div>
+            <div>
+                <h2>Work Experience</h2>
+                <div>
+                    <h3>Junior Web Developer</h3>
+                    <span>Full Time</span>
+                    <span>Dr. Rajkumar's Learning App</span>
+                    <span>Bengaluru</span>
+                    <span>Sep 2021 - Dec 2021</span>
+                </div>
+                <div>
+                    <h3>Web Development Intern</h3>
+                    <span>Internship</span>
+                    <span>IonPixelz Web Solutions</span>
+                    <span>Bengaluru</span>
+                    <span>Sep 2021 - Dec 2021</span>
+                </div>
+                <div>
+                    <h2>Work Experience</h2>
+                    <h3>SEO / SEM Specialist</h3>
+                    <span>Internship</span>
+                    <span>HAAPS</span>
+                    <span>Bengaluru</span>
+                    <span>Sep 2021 - Dec 2021</span>
+                </div>
+            </div>
+            <div>
+                <h2>Education</h2>
+                <div>
+                    <h3>Bachelor in Electronics & Communication</h3>
+                    <span>Full Time</span>
+                    <span>Bangalore Instutute of Technology</span>
+                    <span>Aug 2015 - Dec 2020</span>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/layout/Skills/Skills.tsx
+++ src/layout/Skills/Skills.tsx
@@ -1,6 +1,27 @@
+import { Icon } from "../../components/Icon/Icon";
+
 function Skills(){
     return(
-        <div>Skills</div>
+        <div>
+            <div>
+                <h2>My Tech Stack</h2>
+                <p> Technologies I've been working with recently</p>
+            </div>
+            <div>
+                <Icon height="120" width="120" viewBox="0 0 120 120" iconId="htmlIcon"/>
+                <Icon height="120" width="120" viewBox="0 0 120 120" iconId="cssIcon"/>
+                <Icon height="120" width="120" viewBox="0 0 120 120" iconId="jsIcon"/>
+                <Icon height="120" width="120" viewBox="0 0 120 120" iconId="reactIcon"/>
+                <Icon height="120" width="120" viewBox="0 0 120 120" iconId="reduxIcon"/>
+                <Icon height="120" width="120" viewBox="0 0 120 120" iconId="bootstrapIcon"/>
+                <Icon height="120" width="120" viewBox="0 0 120 120" iconId="tailwindIcon"/>
+                <Icon height="120" width="120" viewBox="0 0 120 120" iconId="sassIcon"/>
+                <Icon height="120" width="120" viewBox="0 0 120 120" iconId="gitIcon"/>
+                <Icon height="120" width="120" viewBox="0 0 120 120" iconId="greensockIcon"/>
+                <Icon height="120" width="120" viewBox="0 0 120 120" iconId="vscodeIcon"/>
+                <Icon height="120" width="120" viewBox="0 0 120 120" iconId="githubIcon"/>
+            </div>
+        </div>
     )
 }
 
