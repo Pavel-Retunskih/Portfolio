11229ed63904c0fd1c7b78e5fb3417c967093de2
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2024-03-16 21:59:25 +0300

    fix svg icons

--- src/layout/Skills/Skills.tsx
+++ src/layout/Skills/Skills.tsx
@@ -15,15 +15,15 @@ export function Skills(){
                     <Icon height="120" width="120" viewBox="0 0 120 120" iconId="htmlIcon"/>
                     <Icon height="120" width="120" viewBox="0 0 120 120" iconId="cssIcon"/>
                     <Icon height="120" width="120" viewBox="0 0 120 120" iconId="jsIcon"/>
-                    <Icon height="120" width="120" viewBox="0 0 120 120" iconId="reactIcon"/>
-                    <Icon height="120" width="120" viewBox="0 0 120 120" iconId="reduxIcon"/>
-                    <Icon height="120" width="120" viewBox="0 0 120 120" iconId="bootstrapIcon"/>
-                    <Icon height="120" width="120" viewBox="0 0 120 120" iconId="tailwindIcon"/>
-                    <Icon height="120" width="120" viewBox="0 0 120 120" iconId="sassIcon"/>
-                    <Icon height="120" width="120" viewBox="0 0 120 120" iconId="gitIcon"/>
-                    <Icon height="120" width="120" viewBox="0 0 120 120" iconId="greensockIcon"/>
-                    <Icon height="120" width="120" viewBox="0 0 120 120" iconId="vscodeIcon"/>
-                    <Icon height="120" width="120" viewBox="0 0 120 120" iconId="githubIcon"/>
+                    <Icon height="120" width="120" viewBox="0 0 113 100" iconId="reactIcon"/>
+                    <Icon height="120" width="120" viewBox="0 0 105 105" iconId="reduxIcon"/>
+                    <Icon height="120" width="120" viewBox="0 0 90 87" iconId="bootstrapIcon"/>
+                    <Icon height="120" width="120" viewBox="10 15 115 115" iconId="tailwindIcon"/>
+                    <Icon height="120" width="120" viewBox="0 0 120 105" iconId="sassIcon"/>
+                    <Icon height="120" width="120" viewBox="0 0 105 105" iconId="gitIcon"/>
+                    <Icon height="120" width="120" viewBox="10 0 120 120" iconId="greensockIcon"/>
+                    <Icon height="120" width="120" viewBox="0 5 105 105" iconId="vscodeIcon"/>
+                    <Icon height="120" width="120" viewBox="0 0 88 88" iconId="githubIcon"/>
                 </FlexWrapper>
             </Container>
         </Section>
